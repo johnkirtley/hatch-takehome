55e7b687f8e80b267c4ca40fe7afd8acfe4a2624
johnkirtley <kirtleyj16@gmail.com>
2020-05-27 12:17:43 -0700

    :sparkles: Login UI Complete
    Login UI is finished. Need to add functionality next.

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { Form, Input, Button } from 'antd';
+
+const layout = {
+	labelCol: { span: 8 },
+	wrapperCol: { span: 8 },
+};
+
+const tailLayout = {
+	wrapperCol: { offset: 8, span: 16 },
+};
 
 export const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div style={{ width: '75%', margin: 'auto', marginTop: '5rem' }}>
+			<h2 style={{ textAlign: 'center' }}>Login</h2>
+			<Form {...layout}>
+				<Form.Item
+					label='Username'
+					name='username'
+					rules={[{ required: true, message: 'Please Enter a Username' }]}>
+					<Input />
+				</Form.Item>
+				<Form.Item
+					label='Password'
+					name='password'
+					rules={[{ required: true, message: 'Please Enter a Password' }]}>
+					<Input />
+				</Form.Item>
+				<Form.Item {...tailLayout}>
+					<Button type='primary' htmlType='submit'>
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
+		</div>
+	);
 };
