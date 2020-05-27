26b6305142c5329e7eeb72ef968eac78f0ac61b1
johnkirtley <kirtleyj16@gmail.com>
2020-05-27 12:42:16 -0700

    :lock: Sign Up Functionality Complete
    Storing credentials in local storage for this assignment.

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button } from 'antd';
 
 const layout = {
@@ -10,31 +10,68 @@ const tailLayout = {
 	wrapperCol: { offset: 8, span: 16 },
 };
 
-export const SignUp = () => {
+const initialState = {
+	username: '',
+	password: '',
+};
+
+export const SignUp = (props) => {
+	const [credentials, setCredentials] = useState(initialState);
+	const [confirmPassword, setConfirmPassword] = useState({
+		confirmPassword: '',
+	});
+
+	const handleChanges = (e) => {
+		setCredentials({
+			...credentials,
+			[e.target.name]: e.target.value,
+		});
+		console.log(credentials);
+	};
+
+	const confirm = (e) => {
+		setConfirmPassword({
+			...confirmPassword,
+			[e.target.name]: e.target.value,
+		});
+		console.log(confirmPassword);
+	};
+
+	const submitCredentials = (e) => {
+		e.preventDefault();
+		if (credentials.password === confirmPassword.confirmPassword) {
+			window.localStorage.setItem('username', credentials.password);
+			window.localStorage.setItem('password', credentials.password);
+			props.history.push('/login');
+		} else {
+			alert('Passwords Do Not Match!');
+		}
+	};
+
 	return (
 		<div style={{ width: '75%', margin: 'auto', marginTop: '5rem' }}>
 			<h2 style={{ textAlign: 'center' }}>Sign Up</h2>
 			<Form {...layout}>
 				<Form.Item
 					label='Username'
-					name='username'
+					htmlFor='username'
 					rules={[{ required: true, message: 'Please Enter a Username' }]}>
-					<Input />
+					<Input onChange={handleChanges} name='username' />
 				</Form.Item>
 				<Form.Item
 					label='Password'
-					name='password'
+					htmlFor='password'
 					rules={[{ required: true, message: 'Please Enter a Password' }]}>
-					<Input />
+					<Input onChange={handleChanges} name='password' type='password' />
 				</Form.Item>
 				<Form.Item
 					label='Confirm Password'
-					name='confirm'
+					htmlFor='confirmPassword'
 					rules={[{ required: true, message: 'Please Reenter Your Password' }]}>
-					<Input />
+					<Input onChange={confirm} name='confirmPassword' type='password' />
 				</Form.Item>
-				<Form.Item {...tailLayout}>
-					<Button type='primary' htmlType='submit'>
+				<Form.Item {...tailLayout} htmlType='submit'>
+					<Button type='primary' onClick={submitCredentials}>
 						Submit
 					</Button>
 				</Form.Item>
