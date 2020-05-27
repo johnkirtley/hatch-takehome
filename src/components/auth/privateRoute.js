0c818832d3022d36da9be002816a406501527ca7
johnkirtley <kirtleyj16@gmail.com>
2020-05-27 13:46:31 -0700

    :lock: Created Private Route Component
    Checks whether or not the auth flag is true before rendering specified component. If auth flag is false, it redirects to Login.

---
+++ src/components/auth/privateRoute.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+export const PrivateRoute = ({ render: Component, ...rest }) => {
+	const [authStatus, setAuthStatus] = useState();
+
+	let authFlag = window.localStorage.getItem('auth');
+
+	useEffect(() => {
+		setAuthStatus(authFlag);
+	}, [authFlag]);
+
+	return (
+		<Route
+			{...rest}
+			render={(props) => {
+				if (authStatus) {
+					return <Component {...props} />;
+				} else {
+					return <Redirect to='/login' />;
+				}
+			}}
+		/>
+	);
+};
