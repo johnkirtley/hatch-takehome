699ac87f0f6331e0a064cb0a5c25e1c0296582de
johnkirtley <kirtleyj16@gmail.com>
2020-05-27 12:51:50 -0700

    :lock: Login Functionality Complete
    Can login and be redirected to applicant page.

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Input, Button } from 'antd';
 
 const layout = {
@@ -10,25 +10,56 @@ const tailLayout = {
 	wrapperCol: { offset: 8, span: 16 },
 };
 
-export const Login = () => {
+const initialState = {
+	username: '',
+	password: '',
+};
+
+export const Login = (props) => {
+	const [login, setLogin] = useState(initialState);
+
+	const handleChanges = (e) => {
+		setLogin({
+			...login,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const submitCredentials = (e) => {
+		e.preventDefault();
+
+		const registeredUsername = window.localStorage.getItem('username');
+		const registeredPassword = window.localStorage.getItem('password');
+
+		if (
+			login.username === registeredUsername &&
+			login.password === registeredPassword
+		) {
+			window.localStorage.setItem('auth', true);
+			props.history.push('/applicants');
+		} else {
+			alert('Invalid Credentials!');
+		}
+	};
+
 	return (
 		<div style={{ width: '75%', margin: 'auto', marginTop: '5rem' }}>
 			<h2 style={{ textAlign: 'center' }}>Login</h2>
 			<Form {...layout}>
 				<Form.Item
 					label='Username'
-					name='username'
+					htmlFor='username'
 					rules={[{ required: true, message: 'Please Enter a Username' }]}>
-					<Input />
+					<Input name='username' onChange={handleChanges} />
 				</Form.Item>
 				<Form.Item
 					label='Password'
-					name='password'
+					htmlFor='password'
 					rules={[{ required: true, message: 'Please Enter a Password' }]}>
-					<Input />
+					<Input name='password' onChange={handleChanges} />
 				</Form.Item>
-				<Form.Item {...tailLayout}>
-					<Button type='primary' htmlType='submit'>
+				<Form.Item {...tailLayout} htmlType='submit'>
+					<Button type='primary' onClick={submitCredentials}>
 						Submit
 					</Button>
 				</Form.Item>
