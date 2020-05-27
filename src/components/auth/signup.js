a273d1eaf3c854ceeee7f9324fbd4aed739d8658
johnkirtley <kirtleyj16@gmail.com>
2020-05-27 12:16:25 -0700

    :sparkles: Sign Up UI Complete
    Sign Up UI is done. Need to add functionality next.

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,5 +1,44 @@
 import React from 'react';
+import { Form, Input, Button } from 'antd';
+
+const layout = {
+	labelCol: { span: 8 },
+	wrapperCol: { span: 8 },
+};
+
+const tailLayout = {
+	wrapperCol: { offset: 8, span: 16 },
+};
 
 export const SignUp = () => {
-	return <div>Signup</div>;
+	return (
+		<div style={{ width: '75%', margin: 'auto', marginTop: '5rem' }}>
+			<h2 style={{ textAlign: 'center' }}>Sign Up</h2>
+			<Form {...layout}>
+				<Form.Item
+					label='Username'
+					name='username'
+					rules={[{ required: true, message: 'Please Enter a Username' }]}>
+					<Input />
+				</Form.Item>
+				<Form.Item
+					label='Password'
+					name='password'
+					rules={[{ required: true, message: 'Please Enter a Password' }]}>
+					<Input />
+				</Form.Item>
+				<Form.Item
+					label='Confirm Password'
+					name='confirm'
+					rules={[{ required: true, message: 'Please Reenter Your Password' }]}>
+					<Input />
+				</Form.Item>
+				<Form.Item {...tailLayout}>
+					<Button type='primary' htmlType='submit'>
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
+		</div>
+	);
 };
